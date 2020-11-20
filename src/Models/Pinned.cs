e870bafcc4431b5f61a7daa39bcc1566aa16550b
Baskaran Govindaras <Baskaran.Govindaras@nintex.com>
2020-11-21 02:58:53 +0800

    Restyled blog

---
+++ src/Models/Pinned.cs
@@ -0,0 +1,13 @@
+﻿namespace BaskaranBlog.Models
+{
+    /// <summary>
+    /// A class to represent pinned posts for main page.
+    /// </summary>
+    public class Pinned
+    {
+        /// <summary>
+        /// Gets or sets the pinned post order.
+        /// </summary>
+        public int Order { get; set; }
+    }
+}
---
+++ src/Models/Post.cs
@@ -0,0 +1,45 @@
+﻿namespace BaskaranBlog.Models
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// A class represents post information.
+    /// </summary>
+    public class Post
+    {
+        /// <summary>
+        /// Gets or sets post year.
+        /// </summary>
+        public string Year { get; set; }
+
+        /// <summary>
+        /// Gets or sets post name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets post url.
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets post date.
+        /// </summary>
+        public string Date { get; set; }
+
+        /// <summary>
+        /// Gets or sets post description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets pinned post information.
+        /// </summary>
+        public Pinned Pinned { get; set; }
+
+        /// <summary>
+        /// Gets or sets post tags.
+        /// </summary>
+        public List<string> Tags { get; set; }
+    }
+}
