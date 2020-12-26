225f5dac9b4a4c6fa479886c79d24d0831ae3fbc
Baskaran Govindaras <Baskaran.Govindaras@nintex.com>
2020-12-26 13:25:37 +0800

    Corrected styling

--- src/Program.cs
+++ src/Program.cs
@@ -13,8 +13,8 @@ namespace BaskaranBlog
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-            builder.Services.AddGoogleAnalytics("G-7M86LVMGM4");
 
+            builder.Services.AddGoogleAnalytics("G-7M86LVMGM4");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
