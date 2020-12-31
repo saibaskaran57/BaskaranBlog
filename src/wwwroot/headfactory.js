c24a60a20a0b6177069d0c0e42b948d1905cf743
Baskaran Govindaras <Baskaran.Govindaras@nintex.com>
2020-12-31 23:44:29 +0800

    Added headerfactory.js

---
+++ src/wwwroot/headfactory.js
@@ -0,0 +1,11 @@
+﻿window.headfactory = {
+    setTitle: function (title) {
+        document.title = title;
+    },
+    setMetadata: function (key, content) {
+        document.querySelector(`meta[name="${key}"]`).setAttribute("content", content);
+    },
+    setGraphMetadata: function (key, content) {
+        document.querySelector(`meta[property="${key}"]`).setAttribute("content", content);
+    },
+};
