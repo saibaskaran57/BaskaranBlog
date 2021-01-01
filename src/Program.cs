59b979e22d3c039d668f68b6b3da7550aba70b9b
Baskaran Govindaras <Baskaran.Govindaras@nintex.com>
2021-01-01 18:58:04 +0800

    Added meta tags for each pages

--- src/Program.cs
+++ src/Program.cs
@@ -6,6 +6,7 @@ namespace BaskaranBlog
     using Blazor.Analytics;
     using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
     using Microsoft.Extensions.DependencyInjection;
+    using Toolbelt.Blazor.Extensions.DependencyInjection;
 
     public class Program
     {
@@ -13,10 +14,9 @@ namespace BaskaranBlog
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
-            builder.Services.AddGoogleAnalytics("G-7M86LVMGM4");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddGoogleAnalytics("G-7M86LVMGM4");
+            builder.Services.AddHeadElementHelper();
             await builder.Build().RunAsync();
         }
     }
